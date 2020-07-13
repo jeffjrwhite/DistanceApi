8854b53a7d31ed989631856f762ab4416f120d83
jeffjrwhite <jeffjrwhite@gmail.com>
2020-07-13 15:14:05 +0200

    Initial Commit

---
+++ src/main/scala/com/ynap/dpetapi/Main.scala
@@ -0,0 +1,9 @@
+package com.ynap.dpetapi
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    DpetapiServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
