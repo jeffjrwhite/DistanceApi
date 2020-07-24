18c2b2cd1e7d641685fb16c88c558933a6aabb4f
jeffjrwhite <jeffjrwhite@gmail.com>
2020-07-24 12:51:47 +0200

    Push of code to test GIT connectivity.

--- src/main/scala/com/ynap/dpetapi/endpoints/DivisionsHandlerImpl.scala
+++ src/main/scala/com/ynap/dpetapi/endpoints/DivisionsHandlerImpl.scala
@@ -7,26 +7,39 @@ import com.ynap.dpetapi.endpoints.divisions.{DivisionsHandler, GetDivisionsRespo
 import com.ynap.dpetapi.endpoints.definitions.DivisionsResponse
 import com.ynap.dpetapi._
 import doobie.util.transactor.Transactor
+import io.circe.Encoder
+import io.circe.syntax._
+
+import scala.util.{Failure, Success, Try}
 
 class DivisionsHandlerImpl[F[_] : Applicative](xa: Transactor[IO]) extends DivisionsHandler[F] {
   override def getDivisions(respond: GetDivisionsResponse.type)(): F[GetDivisionsResponse] = {
     import doobie.implicits._
-    /**
-     * Implicit classes and imports used for JSON "pretty" printing
-     */
-    implicit val formats = net.liftweb.json.DefaultFormats
-    import net.liftweb.json.Serialization.write
+//    /**
+//     * Implicit classes and imports used for JSON "pretty" printing
+//     */
+//    implicit val formats = net.liftweb.json.DefaultFormats
+//    import net.liftweb.json.Serialization.write
 
+    implicit val encodeFieldType: Encoder[Division] =
+      Encoder.forProduct2("id", "name")(Division.unapply(_).get)
+
+    //val rs2 = DivisionQuery.search().to[List].transact(xa).unsafeRunSync.tail.head.Descrizione
     var jsonList = for {
       division <- DivisionQuery.search().to[List].transact(xa).unsafeRunSync
     } yield {
-      val jsonStr = write(division)
+      val jsonStr = division.asJson.noSpaces
       jsonStr
     }
 
-    for {
-      message <- s"${jsonList.mkString("[", ",", "]")}".pure[F]
-    } yield respond.Ok(DivisionsResponse(message))
+    val isJson: IndexedSeq[io.circe.Json] = for( a <- 1 to jsonList.length-1) yield jsonList(a)
+//    for {
+//      list <- jsonList //.pure[F]
+//      //message <- list.asJson
+//      //s"${jsonList.mkString("[", ",", "]")}".pure[F]
+//      //Option[IndexedSeq[io.circe.Json]]
+//    } yield
+      respond.Ok(DivisionsResponse(Some(isJson)))
   }
 
 }
