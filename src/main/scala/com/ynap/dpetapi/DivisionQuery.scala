f1b84e21e691ac7c331f2fbe641475c09345cf5f
jeffjrwhite <jeffjrwhite@gmail.com>
2020-07-27 15:01:09 +0200

    Add optional ID parameter to API schema on Get Divisions Add get Division By Id to Get Divisions query. Add record count to GetDivisionsResponse Resolve Get Divisions data type for a Json List. All working.

--- src/main/scala/com/ynap/dpetapi/DivisionQuery.scala
+++ src/main/scala/com/ynap/dpetapi/DivisionQuery.scala
@@ -7,20 +7,21 @@ case class Division(ID_Divisione: String, Descrizione: String)
 object DivisionQuery {
 
   // search account
-  def search(): doobie.Query0[Division] = {
-    sql"""
-         |SELECT ID_Divisione, Descrizione FROM Divisione
-       """.stripMargin
-      .query[Division]
-  }
+  def search(id: Option[String]): doobie.Query0[Division] = {
 
-  // search with id
-  def searchWithId(id: String): doobie.Query0[Division] = {
-    sql"""
-         |SELECT ID_Divisione, Descrizione FROM Divisione
-         |WHERE ID_Divisione = $id
+    id match {
+      case Some(divId) =>
+        sql"""
+             |SELECT ID_Divisione, Descrizione FROM Divisione
+             |WHERE ID_Divisione = ${divId}
        """.stripMargin
-      .query[Division]
+          .query[Division]
+      case None =>
+          sql"""
+               |SELECT ID_Divisione, Descrizione FROM Divisione
+       """.stripMargin
+            .query[Division]
+    }
   }
 
 }
--- src/main/scala/com/ynap/dpetapi/endpoints/DivisionsHandlerImpl.scala
+++ src/main/scala/com/ynap/dpetapi/endpoints/DivisionsHandlerImpl.scala
@@ -3,6 +3,7 @@ package com.ynap.dpetapi.endpoints
 import cats.Applicative
 import cats.effect.IO
 import cats.implicits._
+import doobie.implicits._
 import com.ynap.dpetapi.endpoints.divisions.{DivisionsHandler, GetDivisionsResponse}
 import com.ynap.dpetapi.endpoints.definitions.DivisionsResponse
 import com.ynap.dpetapi._
@@ -10,36 +11,28 @@ import doobie.util.transactor.Transactor
 import io.circe.Encoder
 import io.circe.syntax._
 
-import scala.util.{Failure, Success, Try}
-
 class DivisionsHandlerImpl[F[_] : Applicative](xa: Transactor[IO]) extends DivisionsHandler[F] {
-  override def getDivisions(respond: GetDivisionsResponse.type)(): F[GetDivisionsResponse] = {
-    import doobie.implicits._
-//    /**
-//     * Implicit classes and imports used for JSON "pretty" printing
-//     */
-//    implicit val formats = net.liftweb.json.DefaultFormats
-//    import net.liftweb.json.Serialization.write
 
-    implicit val encodeFieldType: Encoder[Division] =
-      Encoder.forProduct2("id", "name")(Division.unapply(_).get)
+  implicit val encodeFieldType: Encoder[Division] =
+    Encoder.forProduct2("ID_Divisione", "Descrizione")(Division.unapply(_).get)
 
-    //val rs2 = DivisionQuery.search().to[List].transact(xa).unsafeRunSync.tail.head.Descrizione
-    var jsonList = for {
-      division <- DivisionQuery.search().to[List].transact(xa).unsafeRunSync
+  override def getDivisions(respond: GetDivisionsResponse.type)(id: Option[String] = None): F[GetDivisionsResponse] = {
+
+    var jsonList: List[io.circe.Json] = for {
+      division <- DivisionQuery.search(id).to[List].transact(xa).unsafeRunSync
     } yield {
-      val jsonStr = division.asJson.noSpaces
+      val jsonStr = division.asJson
       jsonStr
     }
+    val isJson: IndexedSeq[io.circe.Json] = for {
+      a <- 0 to jsonList.length-1
+    } yield {
+        jsonList(a)
+    }
+    for {
+      list <- isJson.pure[F]
+    } yield respond.Ok(DivisionsResponse(Some(isJson.length), Some(list)))
 
-    val isJson: IndexedSeq[io.circe.Json] = for( a <- 1 to jsonList.length-1) yield jsonList(a)
-//    for {
-//      list <- jsonList //.pure[F]
-//      //message <- list.asJson
-//      //s"${jsonList.mkString("[", ",", "]")}".pure[F]
-//      //Option[IndexedSeq[io.circe.Json]]
-//    } yield
-      respond.Ok(DivisionsResponse(Some(isJson)))
   }
 
 }
